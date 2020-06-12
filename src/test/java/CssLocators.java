2f0cde8fe261cfa50bcde5cb451989ed2d4bc37c
ruk515 <jekabsons.u@gmail.com>
2020-06-12 14:43:08 +0300

    update

--- src/test/java/CssLocators.java
+++ src/test/java/CssLocators.java
@@ -5,3 +5,7 @@
  $$ - all links available on the page will be returned from DOM.
 
  $$ ("nav#navbar a.nav-item [href='/downloads']")
+
+ #id	        #firstname	        Selects the element with id="firstname"
+ [attribute]	[target]	        Selects all elements with a target attribute
+*/
