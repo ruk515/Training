f7059c75dc9cf7be82b9a3f1239a9a50c4c21d35
ruk515 <jekabsons.u@gmail.com>
2020-06-12 14:25:49 +0300

    update

---
+++ src/test/java/CssLocators.java
@@ -0,0 +1,7 @@
+/**
+
+ CSS locators
+ $ - in case of repeated tags, the first tag found is returned.
+ $$ - all links available on the page will be returned from DOM.
+
+ $$ ("nav#navbar a.nav-item [href='/downloads']")
